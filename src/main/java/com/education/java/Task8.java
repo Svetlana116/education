17d781071b005bc2e1548b680fe1048d370a84cb
Svetlana <36298894+tailer16@users.noreply.github.com>
2021-08-22 21:40:52 +0300

    added 8 task

--- src/main/java/com/education/java/Task8.java
+++ src/main/java/com/education/java/Task8.java
@@ -1,7 +1,16 @@
 package com.education.java;
 
+import java.util.Scanner;
+
 public class Task8 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int f = scanner.nextInt();
+        int r = 1;
 
+        for (int i = 1; i <= f; i++){
+            r *= i;
+        }
+        System.out.printf("Factorial %s = %s", f, r);
     }
 }
